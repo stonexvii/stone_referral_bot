109b2ca163bce9ef07d1620ce5575318a3cd28d3
STONE <17th@inbox.ru>
2025-10-27 13:11:46 +0300

    fix: catch_photo_handler

--- handlers/admin.py
+++ handlers/admin.py
@@ -10,10 +10,13 @@ admin_router.message.middleware(AdminMiddleware())
 
 @admin_router.message(F.photo)
 async def update_pict(message: Message):
-    await requests.add_portfolio(
-        file_id=message.photo[0].file_id,
-        desc=message.caption,
-    )
+    msg_text = message.photo[0].file_id
+    if message.caption:
+        await requests.add_portfolio(
+            file_id=message.photo[0].file_id,
+            desc=message.caption,
+        )
+        msg_text = 'Фото в портфолио добавлено'
     await message.answer(
-        text='Фото в портфолио добавлено'
+        text=msg_text,
     )
